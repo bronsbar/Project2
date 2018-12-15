33586686175911c77512418029b6ba28c0f2f38b
Bart Bronselaer <bart.bronselaer@me.com>
2018-12-15 07:04:46 +0100

    Initial Commit

---
+++ Project2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project2
+//
+//  Created by Bart Bronselaer on 15/12/2018.
+//  Copyright © 2018 Bart Bronselaer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Bart Bronselaer on 15/12/2018.
+//  Copyright © 2018 Bart Bronselaer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
